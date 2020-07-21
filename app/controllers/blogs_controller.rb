05ab0f2d65ca1973809d4df3dc5f089a3977ac5b
ryujihagiwara <dragon7103next@icloud.com>
2020-07-21 17:38:42 +0900

    [Finish]4-7 削除機能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -31,8 +31,12 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog)
   end
 
-  def desstory
-    
+  def destroy
+    blog=Blog.find(params[:id])
+
+    blog.destroy
+
+    redirect_to blogs_path
   end
 
   private
